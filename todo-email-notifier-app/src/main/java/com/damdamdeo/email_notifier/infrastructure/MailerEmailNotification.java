3a69cf754bdcf1f98dfbc7119252b6208ff5fa70
Damien <damien.clementdhuart@gmail.com>
2019-07-15 00:00:22 +0200

    wip

--- todo-email-notifier-app/src/main/java/com/damdamdeo/email_notifier/infrastructure/MailerEmailNotification.java
+++ todo-email-notifier-app/src/main/java/com/damdamdeo/email_notifier/infrastructure/MailerEmailNotification.java
@@ -7,9 +7,13 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class MailerEmailNotification implements EmailNotifier {
+
+    private final static Logger LOGGER = Logger.getLogger(MailerEmailNotification.class.getName());
 
     @ConfigProperty(name = "sendTo")
     String sendTo;
@@ -19,6 +23,7 @@ public class MailerEmailNotification implements EmailNotifier {
 
     @Override
     public void notify(final String content, final String subject) {
+        LOGGER.log(Level.INFO, "email notification with subject ''{0}'' and content ''{1}''", new Object[] {subject, content});
         mailer.send(Mail.withHtml(sendTo, subject, content));
     }
 
--- todo-email-notifier-app/src/test/java/com/damdamdeo/email_notifier/EventStoreEventConsumerTest.java
+++ todo-email-notifier-app/src/test/java/com/damdamdeo/email_notifier/EventStoreEventConsumerTest.java
@@ -1,12 +1,17 @@
 package com.damdamdeo.email_notifier;
 
+import com.damdamdeo.email_notifier.domain.EmailNotifier;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.builder.RequestSpecBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
 public class EventStoreEventConsumerTest {
@@ -17,15 +22,37 @@ public class EventStoreEventConsumerTest {
     @Inject
     EntityManager em;
 
+    @Inject
+    EmailNotifier emailNotifier;
+
     @BeforeEach
     @Transactional
     public void setup() {
+        given(new RequestSpecBuilder().setBaseUri("http://localhost").setPort(8025).build())
+                .when()
+                .delete("/api/v1/messages")
+                .then()
+                .statusCode(200);
         em.createQuery("DELETE FROM TodoEntity").executeUpdate();
         em.createQuery("DELETE FROM KafkaEventEntity").executeUpdate();
     }
-//l'email en localhost ne fonctionne pas ... passer par l'api specifqiue fourni par quarkus !!!
-//    Quoi que non car en test e2e cela ne marchera pas ...
-//    @Test
+
+    @Test
+    public void should_send_email() {
+        // Given
+        emailNotifier.notify("content", "subject");
+
+        // Then
+        given(new RequestSpecBuilder().setBaseUri("http://localhost").setPort(8025).build())
+                .when()
+                .get("/api/v1/messages")
+                .then()
+                .statusCode(200)
+                .body("[0].Content.Headers.Subject[0]", equalTo("subject"))
+                .body("[0].MIME.Parts[0].MIME.Parts[0].Body", equalTo("content"));
+    }
+
+    @Test
     public void should_consume_todo_created_event_and_todo_marked_as_completed_event() throws Exception {
         // When
         kafkaDebeziumProducer.produce("TodoCreatedEvent.json");
@@ -43,8 +70,8 @@ public class EventStoreEventConsumerTest {
 //                .body("version", equalTo(0));
 
         // When
-        kafkaDebeziumProducer.produce("TodoMarkedAsCompletedEvent.json");
-        Thread.sleep(1000);
+//        kafkaDebeziumProducer.produce("TodoMarkedAsCompletedEvent.json");
+//        Thread.sleep(1000);
 
         // Then
 //        given()
