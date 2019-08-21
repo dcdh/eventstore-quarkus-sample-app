c1e602f7882f1d0fa8509af45cbb31d3dc0ce8e9
Damien <damien.clementdhuart@gmail.com>
2019-08-21 02:58:26 +0200

    fix build native image

--- todo-write-app/build_native.sh
+++ todo-write-app/build_native.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export GRAALVM_HOME=/home/damien/Documents/03_projets/04_tools/graalvm-ce-19.0.2
+export GRAALVM_HOME=/home/damien/Documents/03_projets/04_tools/graalvm-ce-19.2.0
+export PATH=/home/damien/Documents/03_projets/04_tools/graalvm-ce-19.2.0/bin:/usr/local/bin:/usr/local/sbin/:/usr/bin
+export JAVA_HOME=/home/damien/Documents/03_projets/04_tools/graalvm-ce-19.2.0
 ./mvnw package -Pnative
