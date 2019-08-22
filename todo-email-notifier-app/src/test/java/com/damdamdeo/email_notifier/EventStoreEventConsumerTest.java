db12adcfda65c53072ce77e9d2bff22d11c76739
Damien <damien.clementdhuart@gmail.com>
2019-08-23 00:37:02 +0200

    add audit tests

--- todo-email-notifier-app/src/test/java/com/damdamdeo/email_notifier/EventStoreEventConsumerTest.java
+++ todo-email-notifier-app/src/test/java/com/damdamdeo/email_notifier/EventStoreEventConsumerTest.java
@@ -1,8 +1,12 @@
 package com.damdamdeo.email_notifier;
 
 import com.damdamdeo.email_notifier.domain.EmailNotifier;
+import com.damdamdeo.email_notifier.domain.TodoStatus;
+import com.damdamdeo.email_notifier.infrastructure.TodoEntity;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.builder.RequestSpecBuilder;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.query.AuditEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +14,13 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 public class EventStoreEventConsumerTest {
@@ -37,6 +44,10 @@ public class EventStoreEventConsumerTest {
                 .statusCode(200);
         em.createQuery("DELETE FROM TodoEntity").executeUpdate();
         em.createQuery("DELETE FROM KafkaEventEntity").executeUpdate();
+
+        em.createNativeQuery("DELETE FROM todoentity_aud").executeUpdate();
+        em.createNativeQuery("DELETE FROM revinfo").executeUpdate();
+        em.createNativeQuery("ALTER SEQUENCE public.hibernate_sequence RESTART WITH 1");
     }
 
     @Test
@@ -82,6 +93,17 @@ public class EventStoreEventConsumerTest {
                 .statusCode(200)
                 .body("[0].Content.Headers.Subject[0]", equalTo("Todo marked as completed"))
                 .body("[0].MIME.Parts[0].MIME.Parts[0].Body", containsString("lorem ipsum"));
+
+        // Then Auditing
+        final List<TodoEntity> todos = AuditReaderFactory.get(em)
+                .createQuery()
+                .forRevisionsOfEntity(TodoEntity.class, true, true)
+                .add(AuditEntity.id().eq("todoId"))
+                .getResultList();
+
+        assertEquals(2, todos.size());
+        assertThat(todos).containsExactly(new TodoEntity("todoId", "lorem ipsum", TodoStatus.IN_PROGRESS, "873ecba4-3f2e-4663-b9f1-b912e17bfc9b", 0l),
+                new TodoEntity("todoId", "lorem ipsum", TodoStatus.COMPLETED, "27f243d6-ba3a-468f-8435-4537e86ae64b", 1l));
     }
 
 }
--- todo-query-app/src/test/java/com/damdamdeo/todo/EventStoreEventConsumerTest.java
+++ todo-query-app/src/test/java/com/damdamdeo/todo/EventStoreEventConsumerTest.java
@@ -1,15 +1,23 @@
 package com.damdamdeo.todo;
 
+import com.damdamdeo.todo.domain.TodoStatus;
+import com.damdamdeo.todo.infrastructure.TodoEntity;
 import com.jayway.restassured.module.jsv.JsonSchemaValidator;
 import io.quarkus.test.junit.QuarkusTest;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.query.AuditEntity;
 import org.junit.jupiter.api.*;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
 public class EventStoreEventConsumerTest {
@@ -25,6 +33,10 @@ public class EventStoreEventConsumerTest {
     public void setup() {
         em.createQuery("DELETE FROM TodoEntity").executeUpdate();
         em.createQuery("DELETE FROM KafkaEventEntity").executeUpdate();
+
+        em.createNativeQuery("DELETE FROM todoentity_aud").executeUpdate();
+        em.createNativeQuery("DELETE FROM revinfo").executeUpdate();
+        em.createNativeQuery("ALTER SEQUENCE public.hibernate_sequence RESTART WITH 1");
     }
 
     @Test
@@ -58,6 +70,17 @@ public class EventStoreEventConsumerTest {
                 .body("description", equalTo("lorem ipsum"))
                 .body("todoStatus", equalTo("COMPLETED"))
                 .body("version", equalTo(1));
+
+        // Then Auditing
+        final List<TodoEntity> todos = AuditReaderFactory.get(em)
+                .createQuery()
+                .forRevisionsOfEntity(TodoEntity.class, true, true)
+                .add(AuditEntity.id().eq("todoId"))
+                .getResultList();
+
+        assertEquals(2, todos.size());
+        assertThat(todos).containsExactly(new TodoEntity("todoId", "lorem ipsum", TodoStatus.IN_PROGRESS, "873ecba4-3f2e-4663-b9f1-b912e17bfc9b", 0l),
+                new TodoEntity("todoId", "lorem ipsum", TodoStatus.COMPLETED, "27f243d6-ba3a-468f-8435-4537e86ae64b", 1l));
     }
 
 }
