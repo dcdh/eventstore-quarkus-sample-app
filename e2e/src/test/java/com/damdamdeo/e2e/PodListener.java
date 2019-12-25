4c4aad359b53ba885ca7cb408a2f11849dc7311d
Damien <damien.clementdhuart@gmail.com>
2019-12-25 20:40:28 +0100

    improve log + another way to send cmd
    I would like to find an another way to send command because I need to exec multiple commands and method exec(var1,...) is not efficient.

--- e2e/src/test/java/com/damdamdeo/e2e/PodListener.java
+++ e2e/src/test/java/com/damdamdeo/e2e/PodListener.java
@@ -5,19 +5,26 @@ import okhttp3.Response;
 
 public final class PodListener implements ExecListener {
 
+    private final String podName;
+
+    public PodListener(final String podName) {
+        this.podName = podName;
+    }
+
     @Override
     public void onOpen(final Response response) {
-        System.out.println("shell open");
+        System.out.println("shell open " + podName);
     }
 
     @Override
     public void onFailure(final Throwable t, Response response) {
-        System.err.println("shell barfed");
+        System.err.println("shell barfed " + podName);
+        System.err.println(t.getMessage());
     }
 
     @Override
     public void onClose(final int code, final String reason) {
-        System.out.println("The shell will now close.");
+        System.out.println("The shell will now close." + podName);
     }
 
 }
--- e2e/src/test/java/com/damdamdeo/e2e/SystemOutCallback.java
+++ e2e/src/test/java/com/damdamdeo/e2e/SystemOutCallback.java
@@ -2,15 +2,21 @@ package com.damdamdeo.e2e;
 
 import io.fabric8.kubernetes.client.Callback;
 
+import java.util.Optional;
+
 public final class SystemOutCallback implements Callback<byte[]> {
 
     private String data;
 
     @Override
     public void call(byte[] data) {
-        this.data = new String(data)
-                .replace("\n", "")
-                .replace("\r", "");
+        Optional.of(new String(data))
+                .map(d -> d.replace("\n", ""))
+                .map(d -> d.replace("\r", ""))
+                .ifPresent(d ->  {
+                    System.out.println(d);
+                    this.data = d;
+                });
     }
 
     public String getData() {
