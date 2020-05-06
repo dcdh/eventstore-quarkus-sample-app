25e2245428706f01b77b865b89545ba8f966db4d
dcdh <damien.clementdhuart@gmail.com>
2020-05-06 23:05:12 +0200

    immutable dtos

--- todo-write-app/src/main/java/com/damdamdeo/todo/interfaces/TodoDTO.java
+++ todo-write-app/src/main/java/com/damdamdeo/todo/interfaces/TodoDTO.java
@@ -4,20 +4,20 @@ import com.damdamdeo.todo.domain.api.Todo;
 import com.damdamdeo.todo.domain.api.TodoStatus;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
+import java.util.Objects;
+
 @RegisterForReflection
-public class TodoDTO {
+public final class TodoDTO {
 
-    public String todoId;
+    public final String todoId;
 
-    public String description;
+    public final String description;
 
-    public TodoStatus todoStatus;
+    public final TodoStatus todoStatus;
 
-    public Boolean canMarkTodoAsCompleted;
+    public final Boolean canMarkTodoAsCompleted;
 
-    public Long version;
-
-    public TodoDTO() {}
+    public final Long version;
 
     public TodoDTO(final Todo todo) {
         this.todoId = todo.todoId();
@@ -29,43 +29,20 @@ public class TodoDTO {
         this.version = todo.version();
     }
 
-    public String getTodoId() {
-        return todoId;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TodoDTO todoDTO = (TodoDTO) o;
+        return Objects.equals(todoId, todoDTO.todoId) &&
+                Objects.equals(description, todoDTO.description) &&
+                todoStatus == todoDTO.todoStatus &&
+                Objects.equals(canMarkTodoAsCompleted, todoDTO.canMarkTodoAsCompleted) &&
+                Objects.equals(version, todoDTO.version);
     }
 
-    public void setTodoId(String todoId) {
-        this.todoId = todoId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public TodoStatus getTodoStatus() {
-        return todoStatus;
-    }
-
-    public void setTodoStatus(TodoStatus todoStatus) {
-        this.todoStatus = todoStatus;
-    }
-
-    public Boolean getCanMarkTodoAsCompleted() {
-        return canMarkTodoAsCompleted;
-    }
-
-    public void setCanMarkTodoAsCompleted(Boolean canMarkTodoAsCompleted) {
-        this.canMarkTodoAsCompleted = canMarkTodoAsCompleted;
-    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public void setVersion(Long version) {
-        this.version = version;
+    @Override
+    public int hashCode() {
+        return Objects.hash(todoId, description, todoStatus, canMarkTodoAsCompleted, version);
     }
 }
