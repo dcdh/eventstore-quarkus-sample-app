5c00cf23b0e9463044b6e511396290252a97287f
dcdh <damien.clementdhuart@gmail.com>
2020-06-25 23:48:56 +0200

    add docker utility to remove container, free spaces and remove docker networks definitions

--- todo-write-app/run_tests.sh
+++ todo-write-app/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export TESTCONTAINERS_RYUK_DISABLED=true; \
+docker kill $(docker ps -aq); docker rm $(docker ps -aq); docker volume prune -f; docker network prune -f; \
+  export TESTCONTAINERS_RYUK_DISABLED=true; \
   mvn -f ../pom.xml clean install -pl todo-domain-api && \
   mvn -f ../pom.xml clean test -pl todo-domain-api,todo-write-app; \
   docker network prune -f
