5610c0b27d78bbdbbb1c623c648c059de959ac69
dcdh <damien.clementdhuart@gmail.com>
2020-09-06 17:40:17 +0200

    Ensure all applications expose common metrics with jaeger metrics.

---
+++ todo-public-frontend/src/test/java/com/damdamdeo/todo/publicfrontend/infrastructure/MetricsTest.java
@@ -0,0 +1,29 @@
+package com.damdamdeo.todo.publicfrontend.infrastructure;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+@QuarkusTest
+public class MetricsTest {
+
+    @Test
+    public void should_publish_metrics() {
+        // Given
+
+        // When && Then
+        RestAssured
+                .given()
+                .accept("application/json")
+                .when()
+                .get("/metrics")
+                .then()
+                .body("base", notNullValue())
+                .body("vendor", notNullValue())
+                .body("application", notNullValue())
+                .statusCode(200);
+    }
+
+}
