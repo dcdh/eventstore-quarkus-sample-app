0811d7f82c5948fbfd387b08a50b47432a0b8a96
dcdh <damien.clementdhuart@gmail.com>
2020-09-07 08:02:37 +0200

    Return traceId in header response like this I can easily find the request.
    https://github.com/quarkusio/quarkus/issues/11931

---
+++ todo-public-frontend/src/main/java/com/damdamdeo/todo/publicfrontend/interfaces/TraceIdResponseInjector.java
@@ -0,0 +1,30 @@
+package com.damdamdeo.todo.publicfrontend.interfaces;
+
+import io.jaegertracing.internal.JaegerSpanContext;
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class TraceIdResponseInjector implements ContainerResponseFilter {
+
+    @Inject
+    Tracer tracer;
+
+    private static final String TRACE_ID = "traceId";
+
+    @Override
+    public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) throws IOException {
+        final SpanContext spanContext = tracer.scopeManager().active().span().context();
+        if (spanContext instanceof JaegerSpanContext) {
+            responseContext.getHeaders().add(TRACE_ID, ((JaegerSpanContext) spanContext).getTraceId());
+        }
+    }
+
+}
