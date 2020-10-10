2ee73c06abcf08ef8f7127184ac4c8f62d4f7619
dcdh <damien.clementdhuart@gmail.com>
2020-10-10 13:44:39 +0200

    Fix redirection.

--- todo-public-frontend/frontend/src/app/app-routing.module.ts
+++ todo-public-frontend/frontend/src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
