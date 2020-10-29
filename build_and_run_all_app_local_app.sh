71d21f10b689be00a15df5eb8d81e294f7e557ee
dcdh <damien.clementdhuart@gmail.com>
2020-10-29 11:58:25 +0100

    Go Java 11 !

--- build_and_run_all_app_local_app.sh
+++ build_and_run_all_app_local_app.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# https://gist.github.com/ricardozanini/fa65e485251913e1467837b1c5a8ed28
 ########################################################################################################################
 # Build
 ### prepare build
+export GRAALVM_HOME=/usr/lib/jvm/graalvm/
+export JAVA_HOME=${GRAALVM_HOME}
+export PATH=${GRAALVM_HOME}/bin:$PATH
+
 docker kill $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker volume prune -f
